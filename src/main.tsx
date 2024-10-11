e67151efb47620b94946d81b592fa3e8103ff48c
tranhuuvinh1109@gmail.com <tranhuuvinh1109@gmail.com>
2024-10-12 00:37:31 +0700

    0.1-main: fix error build

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import BottomSheetProvider from "./provider/BottomSheet/index.tsx";
 import { Toaster } from "react-hot-toast";
 import App from "./App";
 import "./index.css";
-import LoadingProvider from "./provider/Loading/index.tsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <LoadingProvider>
-          <BottomSheetProvider>
-            <Toaster position="top-center" reverseOrder={false} />
-            <App />
-          </BottomSheetProvider>
-        </LoadingProvider>
+        <Toaster position="top-center" reverseOrder={false} />
+        <App />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
