eb5222ff3bd9d7e72cc69cd7a3907815a4d69b86
tranhuuvinh1109@gmail.com <tranhuuvinh1109@gmail.com>
2024-10-17 22:39:27 +0700

    0.4-main: update load image and display original size image

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -5,12 +5,15 @@ import "swiper/css";
 import "swiper/css/effect-cards";
 import { ImageList } from "../../constant/common";
 import Modal from "../Modal";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { SlideItemType } from "../../type/common";
+import { getDownloadURL, listAll, ref as storageRef } from "firebase/storage";
+import { storage } from "../../provider/Firebase";
 
 const Image = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [itemSelected, setItemSelected] = useState<SlideItemType>();
+  const [listImage, setListImage] = useState<SlideItemType[]>([]);
 
   const handleClose = () => {
     setIsOpen(false);
@@ -24,7 +27,7 @@ const Image = () => {
   const renderImage = useMemo(() => {
     return (
       <Swiper effect={"cards"} grabCursor={true} modules={[EffectCards]} className="mySwiper m-0! max-w-[250px]">
-        {ImageList.map((item, index) => {
+        {listImage.map((item, index) => {
           return (
             <SwiperSlide className="h-[450px] w-full rounded-2xl  bg-white text-black shadow-md" key={index}>
               <div className=" relative flex h-full flex-col items-center  p-4" onClick={() => handleClickImage(item)}>
@@ -42,18 +45,45 @@ const Image = () => {
         })}
       </Swiper>
     );
-  }, [handleClickImage]);
+  }, [handleClickImage, listImage]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      if (listImage.length > 0) {
+        return;
+      }
+      const folderRef = storageRef(storage, "mylove/");
+
+      try {
+        const res = await listAll(folderRef);
+        const urls = await Promise.all(
+          res.items.map(async (item) => {
+            const url = await getDownloadURL(item);
+            return url;
+          })
+        );
+        const additionalItem = urls.map(
+          (item) =>
+            ({
+              image: item,
+              content: "",
+            }) as SlideItemType
+        );
+        setListImage([...ImageList, ...additionalItem]);
+      } catch (error) {
+        console.error("Error fetching images: ", error);
+      }
+    };
+
+    fetchImages();
+  }, [listImage]);
 
   return (
     <>
       <div>{renderImage}</div>
       <Modal isOpen={isOpen} className="min-h-[550px] max-w-full" onClose={handleClose}>
         <div className="py-4">
-          <img
-            src={itemSelected?.image}
-            alt={itemSelected?.image}
-            className=" max-h-[300px] w-full rounded-lg object-cover"
-          />
+          <img src={itemSelected?.image} alt={itemSelected?.image} className=" w-full rounded-lg object-cover" />
         </div>
       </Modal>
     </>
